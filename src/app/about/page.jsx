f2af7af2d36603c3ec25e4817881f642188c44d3
Deepak Mane <deepak.mane123@gmail.com>
2023-06-03 22:28:39 -0700

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='container'>
+            <div className='max-w-6xl mx-auto space-y-4'>
+                <h1 className='text-2xl font-medium text-amber-600'>
+                    About
+                </h1>
+                <h1>
+                    <p>
+                        IMDb (an acronym for Internet Movie
+                        Database)[2] is an online database of
+                        information related to films, television
+                        series, podcasts, home videos, video games, and
+                        streaming content online – including cast,
+                        production crew and personal biographies, plot
+                        summaries, trivia, ratings, and fan and
+                        critical reviews.
+                    </p>
+                    <p>
+                        IMDb began as a fan-operated movie database on
+                        the Usenet group "rec.arts.movies" in 1990, and
+                        moved to the Web in 1993. Since 1998, it has
+                        been owned and operated by IMDb.com, Inc., a
+                        subsidiary of Amazon. As of March 2022, the
+                        database contained some 10.1 million titles
+                        (including television episodes) and 11.5
+                        million person records.
+                    </p>
+                    <p>
+                        Additionally, the site had 83 million
+                        registered users. The site's message boards
+                        were disabled in February 2017. As of 2019,
+                        IMDb is the 52nd most visited website in the
+                        world, according to website ranker Alexa
+                    </p>
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default About
